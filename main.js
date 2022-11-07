176b4b44a1827b7a9fdfa91c2f89a35d7a676dd4
Abdul Mufid <110360966+eabdalmufid@users.noreply.github.com>
2022-11-08 03:55:15 +0700

    New

--- main.js
+++ main.js
@@ -85,7 +85,7 @@ global.loadDatabase = async function loadDatabase() {
 }
 loadDatabase()
 
-global.authFile = `${opts._[0] || 'fangz'}.data.json`
+global.authFile = `${opts._[0] || 'session'}.data.json`
 const { state, saveState } = useSingleFileAuthState(global.authFile)
 
 const connectionOptions = {
